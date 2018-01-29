adfe721069af0190a453af008dda5b33eb0b95be
giacomoguiulfo <gguiulfo@student.42.us.org>
2018-01-28 23:49:44 -0800

    Init ft_tar.c

---
+++ ex02/ft_tar.c
@@ -0,0 +1,56 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_tar.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gguiulfo <gguiulfo@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/01/28 23:23:26 by gguiulfo          #+#    #+#             */
+/*   Updated: 2018/01/28 23:48:55 by gguiulfo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+#include "ft_tar.h"
+
+#define TAR_OPT_LC			(1 << 0)
+#define TAR_OPT_LF			(1 << 1)
+#define TAR_OPT_LP			(1 << 2)
+#define TAR_OPT_LT			(1 << 3)
+#define TAR_OPT_LV			(1 << 4)
+#define TAR_OPT_LX			(1 << 5)
+#define TAR_HAS_OPT_LC(x)	((x) & TAR_OPT_LC)
+#define TAR_HAS_OPT_LF(x)	((x) & TAR_OPT_LF)
+#define TAR_HAS_OPT_LP(x)	((x) & TAR_OPT_LP)
+#define TAR_HAS_OPT_LT(x)	((x) & TAR_OPT_LT)
+#define TAR_HAS_OPT_LV(x)	((x) & TAR_OPT_LV)
+#define TAR_HAS_OPT_LX(x)	((x) & TAR_OPT_LX)
+
+static t_optsdata	g_taropts =
+{
+	"ft_tar", NULL, NULL, NULL, 0, 1, 0, {
+		{'c', NULL, NULL, NULL, NULL, TAR_OPT_LC, 0, NULL, 0, 0},
+		{'f', NULL, NULL, NULL, NULL, TAR_OPT_LF, 0, NULL, 0, 0},
+		{'p', NULL, NULL, NULL, NULL, TAR_OPT_LP, 0, NULL, 0, 0},
+		{'t', NULL, NULL, NULL, NULL, TAR_OPT_LT, 0, NULL, 0, 0},
+		{'v', NULL, NULL, NULL, NULL, TAR_OPT_LV, 0, NULL, 0, 0},
+		{'x', NULL, NULL, NULL, NULL, TAR_OPT_LX, 0, NULL, 0, 0},
+		{0, NULL, NULL, NULL, NULL, 0, 0, NULL, 0, 0}
+	}
+};
+
+int main(int argc, char const *argv[])
+{
+	t_optparser	data;
+
+	data.flags = 0;
+	if (ft_opts((char **)argv, &g_taropts, &data, 1))
+		return (1);
+	if (TAR_HAS_OPT_LC(data.flags) && TAR_HAS_OPT_LT(data.flags))
+		return (TAR_ERR("Can't specify both -c and -t\n"));
+	if (TAR_HAS_OPT_LC(data.flags) && TAR_HAS_OPT_LX(data.flags))
+		return (TAR_ERR("Can't specify both -c and -x\n"));
+	if (TAR_HAS_OPT_LT(data.flags) && TAR_HAS_OPT_LX(data.flags))
+		return (TAR_ERR("Can't specify both -t and -x\n"));
+	return (0);
+}
---
+++ ex02/includes/ft_tar.h
@@ -0,0 +1,70 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_tar.h                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: asyed <asyed@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/01/28 05:05:32 by asyed             #+#    #+#             */
+/*   Updated: 2018/01/28 23:49:03 by gguiulfo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_TAR_H
+# define FT_TAR_H
+# include <stdlib.h>
+# include <stdio.h>
+# include <sys/types.h>
+# include <sys/stat.h>
+# include <unistd.h>
+# include <string.h>
+# include <stdlib.h>
+# include "libft.h"
+
+# define ERR_PARAMS "Not enough paramaters"
+# define TAR_ERR(s, ...)		((fprintf(stderr, s, ##__VA_ARGS__)) ? 1 : 1)
+
+typedef struct	s_tarheader
+{
+	char		name[100];
+	char		mode[8];
+	char		uid[8];
+	char		gid[8];
+	char		size[12];
+	char		mtime[12];
+	char		checksum[8];
+	char		linkflag;
+	char		linkname[100];
+	char		indicator[6];
+	char		version[2];
+	char		uname[32];
+	char		gname[32];
+	char		devicemajor[8];
+	char		deviceminor[8];
+	char		nameprefix[155];
+	char		pad[12];
+}				t_tarheader;
+
+/*
+** utils.c
+*/
+
+int				end_padding(void **test, FILE *destfile);
+int				linkf_handle(char linkflag, FILE *destfile,
+							t_dstr **prefix, char *filename);
+void			ft_suffixnull(char *dest, const char *format,
+							int64_t str, size_t len);
+void			calc_chksum(t_tarheader **tar_h);
+int				add_stats(int fd, t_tarheader **tar_h);
+
+/*
+** ft_archive.c
+*/
+
+int				linkflag(t_tarheader **tar_h, struct stat buf);
+int				add_directory(FILE *destfile, char *filename,
+							t_dstr **prefix);
+int				write_file(FILE *destfile, FILE *src, t_tarheader *tar_h);
+int				add_file(FILE *destfile, char *filename, t_dstr **prefix);
+
+#endif
