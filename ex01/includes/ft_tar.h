0bfc7aa08bc32250bda16395a2f2fbdbc135fa32
Adam Syed <suedadam@gmail.com>
2018-01-28 20:53:34 -0800

    Fix bugs

---
+++ ex01/includes/ft_tar.h
@@ -0,0 +1,57 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_tar.h                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: suedadam <suedadam@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/01/28 05:05:32 by asyed             #+#    #+#             */
+/*   Updated: 2018/01/28 13:56:20 by suedadam         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_TAR_H
+# define FT_TAR_H
+# include <stdlib.h>
+# include <stdio.h>
+# include <sys/types.h>
+# include <sys/stat.h>
+# include <unistd.h>
+# include <string.h>
+# include <stdlib.h>
+# include "libft.h"
+
+// # define DEVICEMAJOR(x) (((x >> 8) & 0xfff) | (unsigned int) (x >> 32) & ~0xfff)
+static void	ft_suffixnull(char *dest, const char *format, int64_t str, size_t len);
+int			add_file(FILE *destfile, char *filename, t_dstr **prefix);
+
+/*
+** File Size: Pad with zeroes instead of spaces...1988 uses spaces.
+** 	For historical reasons, a final NUL or space character should also be used.
+** 	Thus although there are 12 bytes reserved for storing the file size,
+** 	only 11 octal digits can be stored.
+**
+*/
+
+typedef struct	s_tarheader
+{
+	char 		name[100];
+	char 		mode[8];
+	char 		uid[8];
+	char 		gid[8];
+	char 		size[12];
+	char 		mtime[12];
+	char 		checksum[8];
+	char 		linkflag;
+	char 		linkname[100];
+	char 		indicator[6];
+	char 		version[2];
+	char		uname[32];
+	char		gname[32];
+	char		devicemajor[8];
+	char		deviceminor[8];
+	char		nameprefix[155];
+	char 		pad[12];
+} 				t_tarheader;
+
+#endif
